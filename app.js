49fa22476ff99370db2757404565bf980a3aace9
Zack Klinger <zklinger@gmail.com>
2016-03-30 22:57:01 -0700

    STYLE and REFACTOR for submission

--- app.js
+++ app.js
@@ -1,52 +1,53 @@
 // app.js
 angular.module('twitchApp', [])
-
-// allow DI for use in controllers, unit tests
+// Add lodash for use in controllers, unit tests
 .constant('_', window._)
-// use in views, ng-repeat="x in _.range(3)"
+// Add lodash for use in views, ng-repeat="x in _.range(3)"
 .run(function ($rootScope) {
   $rootScope._ = window._;
 })
-// 
+// API Service
 .factory('twitchService', function($http) {
-
-  
+  // Make a Twitch API call
   var twitchService = {
     get: function(channel) {
       return $http.get('https://api.twitch.tv/kraken/streams/' + channel);
     }
   };
-
   return twitchService;
 })
+// Main Controller
 .controller('MainController', function($scope, twitchService, $http) {
   var vm = this;
   
-  vm.favs = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "OgamingSC2", "ESL_SC2", "RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+  vm.favs = ["freecodecamp", "brunofin", "comster404", "storbeck", "terakilobyte", "habathcx", "OgamingSC2", "ESL_SC2", "RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
   vm.streams = [];
   
-  
-  _.each((vm.favs), function(fav) {
-    getStreamByChannel(fav);
-  });
-  
+  // Filter for ordering online streams first
   $scope.onlineOrder = function (item) {
     if (item.stream === null) {
       return 1;
     } else {
       return 0;
     }
-
-//    return ['team.name','id];
-  }
+  }  
   
+  // Make an API call for each channel in favorites list
+  _.each((vm.favs), function(fav) {
+    getStreamByChannel(fav);
+  });
   
+  // Call Twitch API for a channel and add to streams array
   function getStreamByChannel(channel) {
     return twitchService.get(channel).then(function(response) {
       console.info(response.data);
       var data = response.data;
       data.name = channel;
       vm.streams.push(data);
+    }, function(err) {
+      console.log(err);
+      vm.streams.errors = vm.streams.errors || [];
+      vm.streams.errors.push({ name: channel, message: err.data.message});
     });
   }
 
